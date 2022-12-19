67b3e5a0b183a9f759bab844d2ef6e6f823e619c
oscar-bermudez <obermudez@crnova.com>
2022-12-18 23:34:41 -0600

    feat: implementar servicio JSON server , además se agrega compoenente para consultar, se completa acción de guardar y consultar , ambos están funcionando correctamente.

--- proyectofinalangular/src/app/services/da.service.ts
+++ proyectofinalangular/src/app/services/da.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Gasto } from '../Modelos/gasto';
-import { datos }from '../data'
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import Gasto from '../Modelos/gasto';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DAService {
-  all_gastos: Gasto[] = datos;
+  private api_url = 'http://localhost:5500/listaDeGastos'; // si cosumimos un webservice o amazon webservice aqui va esa ruta
+  constructor(private http: HttpClient) {}
 
-
-  constructor() { }
-
-  get_gastos(): Gasto[]{
-    return this.all_gastos;
+  get_gastos(): Observable<Gasto[]> {
+    return this.http.get<Gasto[]>(this.api_url);
   }
+  // add_gasto(gasto: Gasto) {
+  //   gasto._id = new Date(Date.now()).getTime();
+  //   console.log('gasto__', gasto);
+  //   datos.concat(gasto);
+  // }
 
-
+  add_gasto(gasto: Gasto): Observable<Gasto> {
+    return this.http.post<Gasto>(this.api_url, gasto, httpOptions);
+  }
 }
